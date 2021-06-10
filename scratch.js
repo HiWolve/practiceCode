f1ba2c9d9954626d23740d44bdbd4837580c1972
Kreston <kreston.cm@gmail.com>
2021-06-10 11:00:11 -0500

    update

--- scratch.js
+++ scratch.js
@@ -1,1 +1,22 @@
 console.log('Kreston');
+console.log(9);
+
+let upper = function (string) {
+
+  return string.toUpperCase();
+
+}
+
+let lower = function (string) {
+  return string.toLowerCase();
+
+}
+
+let concat = function (str1, str2, cb) {
+  let together = str1 + str2;
+
+  if (cb === undefined) {
+    return together
+  }
+  return cb(together)
+}
